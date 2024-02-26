831afa63e7d57c15c00a304a354afb3bdf20f830
agustin <aguss.97@outlook.com>
2024-02-26 18:27:28 -0300

    starting with variables and data types

---
+++ inicio/VariablesYTiposDeDatos.js
@@ -0,0 +1,24 @@
+//Tipos de datos
+string = "cadena de texto";
+numer = 19;
+boolean = false;
+
+//Tipos de variables
+const numero = 10; //Este valor "10" no va a cambiar ya que las constantes permite almacenar un valor sin que sufra cambios
+var letra = "h"; // "var" define una variable de gran alcance (scope) dentro del programa;
+let verdadero = true; //"let" te permite declarar variables limitando su alcance (scope) al bloque, declaración, o expresión donde se está usando
+
+let ejemploNull = null; //La variable se inicializa nula o vacia
+let ejemploUndefined = undefined; //Temporalmente la variable no tiene valor
+let ejemploNaN = ejemploNull + ejemploUndefined; //Esto da como resultado NaN (Not a Number) o que no es un numero
+
+//prompt permite lanzar mensajes por pantalla que espera se ingrese un valor y dicho valor almacenarlo en una variable
+//let nombre = prompt("ingrese nombre");
+//alert(nombre);
+
+//OPERADORES DE ASIGNACION: Asigna un valor al operando de la izquierda basado en el valor del operando de la derecha
+/*
+    %: Muestra el resto de una division.
+    **: Muestra el exponente de un numero
+
+*/
