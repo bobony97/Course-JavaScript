f68bcceb506eeba777c54110e119952663747ec0
agustin <aguss.97@outlook.com>
2024-02-26 19:28:45 -0300

    add conditionals and operators

---
+++ Capitulo1/condicionales.js
@@ -0,0 +1,13 @@
+let nombre = "pedro";
+
+if (nombre == "lucas") {
+    alert("Tu nombre es malardo")
+}
+
+else if (nombre == "dalto") {
+    alert("tu nombre es buenardo")
+}
+
+else {
+    alert("Tu nombre es otro")
+}
---
+++ Capitulo1/operadores.js
@@ -0,0 +1,12 @@
+//Operadores de comparacion: Comparan 2 expresiones y devuelven un valor booleano que representa la relacion de sus valores
+/*
+= Asignacion
+== Comparacion igualdad en valores
+!= Desigualdad
+=== Compara igualdad(extricto) tanto en tipo de dato como en valor
+!== Extrictamente desigual, tanto en tipo de dato como en valor
+< Menor que
+> Mayor que
+<= Menos igual que
+>= Mayor igual que
+*/
